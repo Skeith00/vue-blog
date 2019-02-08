9bcfb7e1854974398ebd77c32310ab3f7218af2f
Sergi <smdt505@hotmail.com>
2019-02-08 15:17:12 +1000

    Initial commit

---
+++ src/router/Router.vue
@@ -0,0 +1,14 @@
+<script>
+    import Vue from 'vue'
+    import VueRouter from 'vue-router'
+    import Blog from '../components/Blog.vue'
+    import Top from '../components/Top.vue'
+
+    Vue.use(VueRouter)
+
+    export default new VueRouter({
+    routes: [
+        {path: '/', component: Blog},
+        {path: '/top', name: 'Top',  component: Top}]
+    })
+</script>
